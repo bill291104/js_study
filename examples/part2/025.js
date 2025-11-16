32e8cc197037419b0ad3c881a2081a74ef8870cb
bill291104 <bill29123@gmail.com>
2025-11-16 23:15:37 +0900

    javascript 200prob week1

---
+++ examples/part2/025.js
@@ -0,0 +1,9 @@
+console.log(14 & 11);
+console.log(~14);
+console.log(14 | 11);
+console.log(14 ^ 11);
+console.log(2 << 2);
+console.log(14 >> 1);
+console.log(14 >>> 2);
+
+console.log((150).toString(2));
